58d82a642cc439aa5f641c8b1a8294ca0e631cd6
Wiserax.Kar.av <eskercorps@gmail.com>
2020-05-12 14:25:02 +0300

    Settings added

--- app/src/main/java/com/example/testtodoapp/Settings.java
+++ app/src/main/java/com/example/testtodoapp/Settings.java
@@ -1,22 +1,35 @@
 package com.example.testtodoapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
+import com.example.testtodoapp.ui.home.EditTaskActivity;
 import com.example.testtodoapp.ui.home.HomeFragment;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Settings extends AppCompatActivity {
 
     public static int globalRemindersTime = 30;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +43,33 @@ public class Settings extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        final SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        try {
+            int value = Math.abs(Integer.parseInt(prefs.getString("reminder_time1", "30")));
+            globalRemindersTime = value;
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Please enter a number value", Toast.LENGTH_SHORT).show();
+            EditTextPreference reminderTime;
+            onBackPressed();
+            //prefs.getStringSet("reminder_time", Collections.singleton("30"));
+        }
+
+
+    }
+
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
         }
     }
 
