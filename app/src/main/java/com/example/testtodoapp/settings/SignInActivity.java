529a645ba3413aa49e72cf50aaefe9e21896ddfd
Niqsan <niqsan@yandex.ru>
2020-05-17 16:02:41 +0300

    added Sign out button

--- app/src/main/java/com/example/testtodoapp/settings/SignInActivity.java
+++ app/src/main/java/com/example/testtodoapp/settings/SignInActivity.java
@@ -1,8 +1,8 @@
 package com.example.testtodoapp.settings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +22,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
@@ -28,7 +30,6 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class SignInActivity extends AppCompatActivity {
 
     private GoogleSignInClient mGoogleSignInClient;
-    private GoogleSignInAccount account;
     private final static int RC_SIGN_IN = 666;
 
     TextView currentUser;
@@ -46,24 +47,13 @@ public class SignInActivity extends AppCompatActivity {
 
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
-            String personName = acct.getDisplayName();
-            String personGivenName = acct.getGivenName();
-            String personFamilyName = acct.getFamilyName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
-            Uri personPhoto = acct.getPhotoUrl();
-
-
-            MainActivity.email = personEmail;
-
-            currentUser.setText(personEmail);
+            MainActivity.email = acct.getEmail();
+            currentUser.setText(acct.getEmail());
         }
-
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-
 
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
@@ -74,19 +64,27 @@ public class SignInActivity extends AppCompatActivity {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (account == null) {
-                    signIn();
-//                    Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
-                }
+                signIn();
             }
         });
+
+
+        Button signOutButton = findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                currentUser.setText("YOUR MAIL");
+                Toast.makeText(getApplicationContext(), "Sign out", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public String getGmail() {
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
-            String personEmail = acct.getEmail();
-            return personEmail;
+            return acct.getEmail();
         }
         return null;
     }
@@ -95,6 +93,15 @@ public class SignInActivity extends AppCompatActivity {
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                    }
+                });
     }
 
     @Override
@@ -114,10 +121,18 @@ public class SignInActivity extends AppCompatActivity {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+//                Toast.makeText(this, "Successful login", Toast.LENGTH_SHORT).show();
+                finish();
+                Intent homeIntent = new Intent(this, SignInActivity.class);
+                startActivity(homeIntent);
+            } else {
+                Toast.makeText(this, "you are already logged in", Toast.LENGTH_SHORT).show();
+            }
+
             // Signed in successfully, show authenticated UI.
-//            updateUI(account);
-            Intent homeIntent = new Intent(this, Settings.class);
-            startActivity(homeIntent);
+
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
